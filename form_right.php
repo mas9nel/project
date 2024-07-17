442e00eda484e825e3e3cd7680d8e9d64873cb0d
mas9nel <96843046+mas9nel@users.noreply.github.com>
2024-07-17 06:16:56 +0300

    Update form_right.php

--- form_right.php
+++ form_right.php
@@ -2,9 +2,5 @@
 <form method="GET" action="index.php" id="right_form">
     <input type="button" value="Стиль" class="button" id="change_theme">
     
-    <input type="hidden" name="id" value= 0>
-    <input type="hidden" name="in_stok" value="on">
-    <input type="hidden" name="out_stok" value="on">
-    
     <input type="submit" class="button" value="Главная" id="form_right_main">
 </form>
